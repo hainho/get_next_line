216952006e18b557f0593bbb6b41c5fb00e98a70
hainho <hainho9704@gmail.com>
2021-07-26 21:55:55 +0900

    clear issue that read dirty buf

--- get_next_line.c
+++ get_next_line.c
@@ -82,7 +82,7 @@ char	*get_next_line(int fd)
 	flag = read_buf(fd, backup);
 	if (ft_strlen(backup[fd]) == 0)
 		return (NULL);
-	if (is_newline(backup[fd]) == ft_strlen(backup[fd]) - 1)
+	if (backup[fd][ft_strlen(backup[fd]) - 1] == '\n')
 		flag = 0;
 	if (flag == 0)
 	{
