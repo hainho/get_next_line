d810d1a5d1ee045c4500480541bd49e193c8521a
hainho <hainho9704@gmail.com>
2021-07-26 18:49:55 +0900

    modify header file

--- get_next_line.h
+++ get_next_line.h
@@ -29,7 +29,7 @@ char	*get_next_line(int fd);
 int		ft_strlen(char *str);
 char	*ft_strdup(char *s);
 char	*ft_strjoin(char *str1, char *str2);
-void	*ft_memcpy(void *dst, const void *src, size_t n);
-size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+void	*ft_memcpy(void *dst, void *src, size_t n);
+size_t	ft_strlcpy(char *dst, char *src, size_t dstsize);
 
 #endif
